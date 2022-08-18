2083766f34803f498137c21858f1bb6048ebb721
hastomimunthe <hastomi.riduan10@gmail.com>
2022-08-18 17:08:43 +0700

    Initial Commit

---
+++ MC3 Harmony School/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC3 Harmony School
+//
+//  Created by Hastomi Riduan Munthe on 18/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC3 Harmony School/MC3_Harmony_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_Harmony_SchoolApp.swift
+//  MC3 Harmony School
+//
+//  Created by Hastomi Riduan Munthe on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MC3_Harmony_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
